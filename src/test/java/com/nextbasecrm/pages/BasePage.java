8051c91e236d6899638c0c91bdcdf3295e92fc43
yetimme <yetimme@yahoo.com>
2020-05-28 08:09:01 +0100

    Add BasePage.java File

---
+++ src/test/java/com/nextbasecrm/pages/BasePage.java
@@ -0,0 +1,22 @@
+package com.nextbasecrm.pages;
+
+import com.nextbasecrm.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    @FindBy(id = "logo_24_a")
+    public WebElement homePage;
+
+    @FindBy(id = "user-block")
+    public WebElement userMail;
+
+    @FindBy(linkText = "/auth/?logout=yes&backurl=%2Fstream%2F")
+    public WebElement logOut;
+}
