2fce1f6bd0271444ea671fb08694a8ab7b267fcc
yetimme <yetimme@yahoo.com>
2020-05-29 09:14:17 +0100

    Update UserStory7.java

--- src/test/java/com/nextbasecrm/tests/components/UserStory7.java
+++ src/test/java/com/nextbasecrm/tests/components/UserStory7.java
@@ -4,6 +4,7 @@ import com.nextbasecrm.pages.ActivityStreamPage;
 import com.nextbasecrm.pages.LoginPage;
 import com.nextbasecrm.tests.TestBase;
 import com.nextbasecrm.utilities.Driver;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,6 +18,33 @@ public class UserStory7 extends TestBase {
         ActivityStreamPage activityStreamPage = new ActivityStreamPage();
         activityStreamPage.navigateToAnnouncement();
         extentLogger.info("Go To Announcement");
+    }
+
+    @Test
+    public void test2(){
+        extentLogger = report.createTest("Announcement Link Verification");
+        new LoginPage().loginAsHelpDesk();
+        extentLogger.info("Login As Help Desk");
+        ActivityStreamPage activityStreamPage = new ActivityStreamPage();
+        activityStreamPage.navigateToAnnouncement();
+        extentLogger.info("Go To Announcement");
+        Driver.get().switchTo().frame(0);
+        Driver.get().findElement(By.xpath("//body[@contenteditable='true']")).clear();
+        Driver.get().switchTo().parentFrame();
+        activityStreamPage.linkIcon.click();
+        extentLogger.info("Click link icon to create link");
+        String text = "amazon";
+        String url = "https://www.amazon.co.uk/";
+        activityStreamPage.addLinkAfterClick(text,url);
+        extentLogger.info("Add Link");
+        Driver.get().switchTo().frame(0);
+        if(!text.isEmpty()) {
+            Assert.assertTrue(Driver.get().findElement(By.linkText(text)).isDisplayed());
+        }else{
+            Assert.assertTrue(Driver.get().findElement(By.linkText(url)).isDisplayed());
+        }
+        extentLogger.info("Verify link is displayed");
+        extentLogger.pass("PASS");
     }
 
     @Test
