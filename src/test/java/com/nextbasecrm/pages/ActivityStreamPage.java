31c3499a74f5d1198b61bd0960d93a415540635c
yetimme <yetimme@yahoo.com>
2020-05-29 09:09:59 +0100

    Update ActivityStreamPage.java

--- src/test/java/com/nextbasecrm/pages/ActivityStreamPage.java
+++ src/test/java/com/nextbasecrm/pages/ActivityStreamPage.java
@@ -1,5 +1,9 @@
 package com.nextbasecrm.pages;
 
+import com.nextbasecrm.utilities.BrowserUtils;
+import com.nextbasecrm.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -17,11 +21,26 @@ public class ActivityStreamPage extends BasePage{
     @FindBy(id = "bx-b-quote-blogPostForm")
     public WebElement commaIcon;
 
+    @FindBy(css = "#bx-b-link-blogPostForm>span")
+    public WebElement linkIcon;
+
+    @FindBy(id = "linkidPostFormLHE_blogPostForm-text")
+    public WebElement linkText;
+
+    @FindBy(id = "linkidPostFormLHE_blogPostForm-href")
+    public WebElement linkUrl;
+
     @FindBy(css = "blockquote")
     public WebElement blockQuote;
 
     public void navigateToAnnouncement(){
         moreButton.click();
         announcementButton.click();
+    }
+
+    public void addLinkAfterClick(String text, String url){
+        linkText.sendKeys(text);
+        linkUrl.sendKeys(url);
+        Driver.get().findElement(By.id("undefined")).click();
     }
 }
