5c55b377cdf3af4b62422db46e62f6cbe13acbde
yetimme <yetimme@yahoo.com>
2020-06-01 08:37:17 +0100

    Update UserStory9.java

--- src/test/java/com/nextbasecrm/tests/components/UserStory9.java
+++ src/test/java/com/nextbasecrm/tests/components/UserStory9.java
@@ -7,8 +7,11 @@ import com.nextbasecrm.utilities.BrowserUtils;
 import com.nextbasecrm.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class UserStory9 extends TestBase {
 
@@ -30,7 +33,7 @@ public class UserStory9 extends TestBase {
         extentLogger.info("Click filter and search input");
         activityStreamPage.announcementsButtonInFilterAndSearchInput.click();
         extentLogger.info("click announcements as default filter");
-        Assert.assertFalse(activityStreamPage.searchResultsAfterUsingFilterAndSearch.isEmpty());
+        Assert.assertFalse(activityStreamPage.resultsAfterUsingFilterAndSearch.isEmpty());
         extentLogger.info("Verify it can be searched by default filters");
         extentLogger.pass("PASS");
     }
@@ -58,6 +61,33 @@ public class UserStory9 extends TestBase {
         String textOfWrittenMessage = Driver.get().findElement(By.xpath("//div[@class='feed-post-text-block-inner-inner']")).getText();
         Assert.assertTrue(textOfWrittenMessage.contains(text));
         extentLogger.info("Verify that this message is diplayed");
+        extentLogger.pass("PASS");
+    }
+
+    @Test
+    public void test4(){
+        extentLogger = report.createTest("Save Filter functionality verification");
+        new LoginPage().loginAsHelpDesk();
+        extentLogger.info("Login as Help Desker");
+        ActivityStreamPage activityStreamPage = new ActivityStreamPage();
+        activityStreamPage.filterAndSearchInputContainer.click();
+        extentLogger.info("Click on filter and search input");
+        activityStreamPage.saveFilterButtonAfterFilterAndSearch.click();
+        extentLogger.info("Click on save filter button");
+        String name = "example2";
+        activityStreamPage.filterNameInputAfterSaveFilterButton.sendKeys(name);
+        extentLogger.info("name the filter");
+//        List<WebElement> dateDropdownInFilterAndSearch = activityStreamPage.dateDropdownInFilterAndSearch;
+//        dateDropdownInFilterAndSearch.get(0).click();
+//        extentLogger.info("Select one type of date");
+//        List<WebElement> typeDropdownInFilterAndSearch = activityStreamPage.typeDropdownInFilterAndSearch;
+//        typeDropdownInFilterAndSearch.get(0).click();
+//        extentLogger.info("Select one type of type");
+        activityStreamPage.saveButtonAfterSaveFilter.click();
+        activityStreamPage.searchButtonInFilterAndSearch.click();
+        extentLogger.info("Save this filter and search");
+        Assert.assertTrue(Driver.get().findElement(By.xpath("//*[text()='"+name+"'][@class='main-ui-square-item']")).isDisplayed());
+        extentLogger.info("Verify that this filter is displayed");
         extentLogger.pass("PASS");
     }
 }
