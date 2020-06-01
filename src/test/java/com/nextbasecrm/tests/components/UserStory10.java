d6fa1346b1efdecb43f5318a6925ff3a8c6bdddb
frknygt <avfurkanyigit@gmail.com>
2020-06-01 18:25:09 +0100

    add

--- src/test/java/com/nextbasecrm/tests/components/UserStory10.java
+++ src/test/java/com/nextbasecrm/tests/components/UserStory10.java
@@ -31,7 +31,7 @@ public class UserStory10 extends TestBase {
     }
 
     @Test(dataProvider = "testData")
-    public void test1(String username,String password){
+    public void AC1(String username,String password){
         extentLogger=report.createTest("User Story 10 AC:1");
         new LoginPage().login(username,password);
         extentLogger.info("login as a marketing user");
@@ -96,7 +96,7 @@ public class UserStory10 extends TestBase {
 
     }
     @Test(dataProvider = "testData")
-    public void test3(String username,String password){
+    public void AC3(String username,String password){
 
         extentLogger=report.createTest("User Story 10 AC:1");
         new LoginPage().login(username,password);
@@ -120,7 +120,7 @@ public class UserStory10 extends TestBase {
     }
 
     @Test(dataProvider = "testData")
-    public void test4(String username,String password){
+    public void AC4(String username,String password){
 
         extentLogger=report.createTest("User Story 10 AC:1");
         new LoginPage().login(username,password);
--- src/test/java/com/nextbasecrm/tests/components/UserStory4.java
+++ src/test/java/com/nextbasecrm/tests/components/UserStory4.java
@@ -5,11 +5,15 @@ import com.nextbasecrm.tests.TestBase;
 import com.nextbasecrm.utilities.BrowserUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class UserStory4 extends TestBase {
     @Test
-    public void test1(){
+    public void AC1(){
         extentLogger=report.createTest("US 4 ac1-2 test");
 
         new LoginPage().loginAsHelpDesk();
@@ -25,23 +29,43 @@ public class UserStory4 extends TestBase {
 
         String path = System.getProperty("user.dir")+"/"+"src/test/resources/Istanbul.jpg";
 
-        //WebElement uploadFilesAndImages = driver.findElement(By.xpath("(//div[@class='diskuf-uploader'])[1]"));
         WebElement uploadFilesAndImages = driver.findElement(By.xpath("((//table/tbody)[7]//td)[1]"));
-        uploadFilesAndImages.sendKeys(path);
+        //WebElement uploadFilesAndImages =driver.findElement(By.cssSelector(".wd-fa-add-file-light-title-text"));
+        //wait.until(ExpectedConditions.elementToBeClickable(uploadFilesAndImages));
+        uploadFilesAndImages.click();
 
+    }
+
+    @Test
+    public void AC2(){
+        extentLogger=report.createTest("US 4 ac1-2 test");
+
+        new LoginPage().loginAsHelpDesk();
+        extentLogger.info("Login as a help desk");
+
+        WebElement PollButton = driver.findElement(By.xpath("//div[@id='feed-add-post-form-tab']//span[.='Poll']"));
+        PollButton.click();
+        extentLogger.info("Click on poll button");
 
         WebElement addUsersButton = driver.findElement(By.id("feed-add-post-destination-container"));
         addUsersButton.click();
 
         WebElement recentOption = driver.findElement(By.cssSelector(".bx-finder-box-tab.bx-lm-tab-last.bx-finder-box-tab-selected"));
         recentOption.click();
-        driver.findElement(By.xpath("(//div[@class='bx-finder-box-item-t7-name'])[2]")).click();
+        WebElement recentEmployee = driver.findElement(By.xpath("(//div[@class='bx-finder-box-item-t7-name'])[1]"));
+        recentEmployee.click();
+
+        //Assert.assertEquals(listOfEmployees.get(listOfEmployees.size()).getText(),recentEmployee.getText());
 
         WebElement employeeAndDepartmentsOption = driver.findElement(By.cssSelector(".bx-finder-box-tab.bx-lm-tab-department"));
         employeeAndDepartmentsOption.click();
+        WebElement firstEmployee = driver.findElement(By.cssSelector(".bx-finder-company-department-employee-name"));
+        String expectedEmployeeName=firstEmployee.getText();
+        firstEmployee.click();
 
-
-
+        List<WebElement> listOfEmployees = driver.findElements(By.xpath("//span[@id='feed-add-post-destination-item']/span"));
+        int number = listOfEmployees.size();
+        Assert.assertEquals(listOfEmployees.get(number-1).getText(),expectedEmployeeName);
 
     }
 }
