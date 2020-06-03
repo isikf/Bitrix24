e9449695e9f10eb4a62a3eee81b3b0748b91f7f2
MaliYurt7 <mehmetaliyurtseven@gmail.com>
2020-06-03 13:32:44 +0100

    update code

--- src/test/java/com/nextbasecrm/pages/ActivityStreamEvent.java
+++ src/test/java/com/nextbasecrm/pages/ActivityStreamEvent.java
@@ -2,21 +2,21 @@ package com.nextbasecrm.pages;
 
 
 import com.nextbasecrm.utilities.BrowserUtils;
+import com.nextbasecrm.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class ActivityStreamEvent extends BasePage {
 
 
     @FindBy( id = "feed-add-post-form-tab-calendar")
-    WebElement eventTab;
+    public WebElement eventTab;
 
 
 
-    public void navigateToEventTab(){
 
-        BrowserUtils.waitForClickablility(eventTab,5);
-       // navigateToModule("Activity Stream");
-        eventTab.click();
-    }
+
+
 }
